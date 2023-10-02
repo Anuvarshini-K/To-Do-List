492767991ccd7d77872c1430834d966dfb93a3ce
Anuvarshini-K <anuaadhiraa@gmail.com>
2023-10-02 09:11:56 +0530

    updated index.html

--- new.js
+++ new.js
@@ -1,32 +1,30 @@
+function addTask() {
+    const taskInput = document.getElementById('task');
+    const taskText = taskInput.value.trim();
 
-        function addTask() {
-            const taskInput = document.getElementById('task');
-            const taskText = taskInput.value.trim();
+    if (taskText !== '') {
+        const taskList = document.getElementById('taskList');
+        const li = document.createElement('li');
+        li.innerHTML = `
+            <input type="checkbox" onclick="toggleCompleted(this)">
+            <span>${taskText}</span>
+            <span class="cross" onclick="removeTask(this)">X</span>
+        `;
+        taskList.appendChild(li);
+        taskInput.value = '';
+    }
+}
 
-            if (taskText !== '') {
-                const taskList = document.getElementById('taskList');
-                const li = document.createElement('li');
-                li.innerHTML = `
-                    <input type="checkbox" onclick="toggleCompleted(this)">
-                    <span>${taskText}</span>
-                    <button onclick="removeTask(this)">Delete</button>
-                `;
-                taskList.appendChild(li);
-                taskInput.value = '';
-            }
-        }
+function toggleCompleted(checkbox) {
+    const taskText = checkbox.nextElementSibling;
+    if (checkbox.checked) {
+        taskText.classList.add('completed');
+    } else {
+        taskText.classList.remove('completed');
+    }
+}
 
-        function toggleCompleted(checkbox) {
-            const taskText = checkbox.nextElementSibling;
-            if (checkbox.checked) {
-                taskText.classList.add('completed');
-            } else {
-                taskText.classList.remove('completed');
-            }
-        }
-
-        function removeTask(button) {
-            const taskList = document.getElementById('taskList');
-            taskList.removeChild(button.parentElement);
-        }
-    
+function removeTask(cross) {
+    const taskList = document.getElementById('taskList');
+    taskList.removeChild(cross.parentElement);
+}
